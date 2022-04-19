9e1f25a8d3b8d6a1e8c980de868d3c19311dd5af
Amgad Abdelhafez <aabdelhafez@paypal.com>
2022-04-18 17:24:19 -0700

    adding command line args and .env file

--- wake.py
+++ wake.py
@@ -1,121 +1,121 @@
+import sys
 import os
 import time
-
 import schedule
 import urllib3
+from datetime import datetime
+from dotenv import dotenv_values
+from collections import OrderedDict
 from pyvirtualdisplay import Display
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
+
+# Command line parametes:
+# --env: config file with email and password
+# --headless: run headless
+# --verbose: print log 
+
+if '--env' in sys.argv:
+    config = dotenv_values(sys.argv[sys.argv.index('--env') + 1])
+else:
+    os.putenv("username", "John")
+    os.putenv("email", "abc@gmail.com")
+    config = OrderedDict([("username", "user@domain.com"), ("password", "CHANGE_ME!!")])
+
+print(datetime.today().strftime("%Y-%m-%d"), datetime.now().strftime("%H:%M:%S"), "Account:", config["username"])
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-display = Display(visible=0, size=(800, 600))
-display.start()
-
-instances = [
-    {
-        "username": "aabdelhafez@paypal.com",
-        "pass": "Efta7YaDev66^",
-    },
-    {
-        "username": "tbmdemo@protonmail.com",
-        "pass": "Efta7YaSn66^",
-    },
-]
-
-wakeuptime = '08:00'
+# display = Display(visible=0, size=(1400, 800))
+# display.start()
 
 # Signin to instance via headless chromium to wake up.
 def wake(username, passwerd):
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    if '--headless' in sys.argv:
+        chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--disable-extensions')                                                                                                                                  
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-extensions")                                                                                                                                  
+    chrome_options.add_argument("--start-maximized")                                                                                                                                  
+    chrome_options.add_argument("--window-size=1400,800")                                                                                                                                  
 
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.add_experimental_option('useAutomationExtension', False)
+    chrome_options.add_experimental_option("useAutomationExtension", False)
 
-    # chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    os.environ['WDM_SSL_VERIFY'] = '0'
-    os.environ['WDM_LOG_LEVEL'] = '0'
-    # s = Service(ChromeDriverManager().install())
+    os.environ["WDM_SSL_VERIFY"] = "0"
+    os.environ["WDM_LOG_LEVEL"] = "0"
+
+    dev_portal_url = "https://developer.servicenow.com/dev.do#!/home"
+    signon_url = "https://signon.service-now.com/ssologin.do?RelayState=%252Fapp%252Fservicenow_ud%252Fexks6phcbx6R8qjln0x7%252Fsso%252Fsaml%253FRelayState%253Dhttps%25253A%25252F%25252Fdeveloper.servicenow.com%25252Fnavpage.do&redirectUri=&email="
+    
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    # driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
-    
-    instance = "https://signon.service-now.com/ssologin.do?RelayState=%252Fapp%252Fservicenow_ud%252Fexks6phcbx6R8qjln0x7%252Fsso%252Fsaml%253FRelayState%253Dhttps%25253A%25252F%25252Fdeveloper.servicenow.com%25252Fnavpage.do&redirectUri=&email="
-    print("Signing in User:".format(instance))
-    print(username.format(instance))
-    
-    driver.get(instance)
-
-    # Collect HTML Elements we'll be using
-    name_input = driver.find_element(By.ID, "username")
-    name_submit = driver.find_element(By.ID, "usernameSubmitButton")
-    pass_input = driver.find_element(By.ID, "password")
-    login_button = driver.find_element(By.ID, "submitButton")
-
+    driver.get(signon_url)
     # Sign In
-    name_input.click()
-    time.sleep(1)
-    name_input.send_keys(username)
-    print("Filled in Username".format(instance))
-    time.sleep(2)
-    name_submit.click()
-    print("Clicked Username Submit Button".format(instance))
+    # username input box
+    driver.find_element(By.ID, "username").click()
+    driver.find_element(By.ID, "username").send_keys(username)
+    # username submit button
+    driver.find_element(By.ID, "usernameSubmitButton").click()
+    WebDriverWait(driver, 1).until(
+        EC.element_to_be_clickable(driver.find_element(By.ID, "password"))
+    )
+    # password input box
+    driver.find_element(By.ID, "password").click()
+    driver.find_element(By.ID, "password").send_keys(passwerd)
+    # password submit button
+    driver.find_element(By.ID, "submitButton").click()
+    # wait to redirect to dev portal homepage
     WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable(pass_input)
+        EC.url_to_be(dev_portal_url)
     )
-
-    pass_input.click()
-    time.sleep(1)
-    pass_input.send_keys(passwerd)
-    print("Filled in Pass".format(instance))
-
-    time.sleep(2)
-    login_button.click()
-    print("Clicked Login Button".format(instance))
-    print("waiting to redirect to dev portal".format(instance))
-    WebDriverWait(driver, 30).until(
-        EC.url_to_be('https://developer.servicenow.com/dev.do#!/home')
-    )
-    time.sleep(2)
-    # find the acount avatar
+    # wait for header avatar to be available
     avatar_query = "return document.querySelector('dps-app').shadowRoot.querySelector('div').querySelector('header').querySelector('dps-navigation-header').shadowRoot.querySelector('header>div>div>ul>li>dps-login')"
     WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable(driver.execute_script(avatar_query))
     )
+    # click on header avatar
     driver.execute_script(avatar_query).click()
     time.sleep(2)
-
+    # get instance status
     status_query = "return document.querySelector('dps-app').shadowRoot.querySelector('div').querySelector('header').querySelector('dps-navigation-header').shadowRoot.querySelector('header').querySelector('dps-navigation-header-dropdown').querySelector('dps-navigation-login-management').shadowRoot.querySelector('dps-navigation-header-dropdown-content').querySelector('dps-navigation-section').querySelector('dps-navigation-instance-management').shadowRoot.querySelector('dps-content-stack')"
-    instance_status = driver.execute_script(status_query).text
-    print(instance_status.format(instance))
+    instance_status = driver.execute_script(status_query).text.split('\n')[1]
+    print(datetime.today().strftime('%Y-%m-%d'), datetime.now().strftime("%H:%M:%S"), "Instance status:", instance_status)
+
+    # get instance name (only available when instance is online)
+    if instance_status == 'Online':
+        instance_name_query = "return document.querySelector('dps-app').shadowRoot.querySelector('div').querySelector('header').querySelector('dps-navigation-header').shadowRoot.querySelector('header').querySelector('dps-navigation-header-dropdown').querySelector('dps-navigation-login-management').shadowRoot.querySelector('dps-navigation-header-dropdown-content').querySelector('dps-navigation-section').querySelector('dps-navigation-instance-management').shadowRoot.querySelector('ul')"
+        WebDriverWait(driver, 10).until(
+            # we find the instance name by clicking on Manage Password item in the Dev portal header menu
+            # which is the 6th element in that list
+            EC.element_to_be_clickable(driver.execute_script(instance_name_query).find_element(By.XPATH, "li[6]"))
+        )
+        driver.execute_script(instance_name_query).find_element(By.XPATH, "li[6]").click()
+        time.sleep(2)
+        instance_name = driver.execute_script("return document.querySelector('dps-app').shadowRoot.querySelector('div').querySelector('header').querySelector('dps-instance-modal').shadowRoot.querySelector('dps-modal')").text.split('\n')[20]
+        print(datetime.today().strftime('%Y-%m-%d'), datetime.now().strftime("%H:%M:%S"), instance_name)
 
     # Save Img of signin proof.
     # driver.get_screenshot_as_file("capture.png")
-    print("Done, cleaning up.".format(instance))
+    # print(datetime.today().strftime('%Y-%m-%d'), datetime.now().strftime("%H:%M:%S"), "Done, cleaning up.")
     
     # Cleanup active browser.
     driver.quit()
-    display.stop()
-# 
 
-# Loop through instances to wake.
 def sunsup():
-    for inst in instances:
-        wake(inst["username"], inst["pass"])
+    wake(config["username"], config["password"])
 
-
+# wakeuptime = '08:00'
 # # Set Schedule for continuous waking.
 # schedule.every().day.at(wakeuptime).do(sunsup)
 # while True:
 #     schedule.run_pending()
-#     time.sleep(1)
+    # time.sleep(1)
 
 sunsup()
