38c2ea1f1f9d8386ca8921f62cd742f7421497e9
Amgad <amgad@MacBook.lan>
2022-05-09 11:09:00 -0700

    refactor: SN API endpoints

--- wake.py
+++ wake.py
@@ -5,9 +5,11 @@ import getpass
 import schedule
 import urllib3
 from datetime import datetime
+# import dotenv
+# from dotenv import load_dotenv
 from dotenv import dotenv_values
-from collections import OrderedDict
-from pyvirtualdisplay import Display
+# from collections import OrderedDict
+# from pyvirtualdisplay import Display
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -28,20 +30,23 @@ from cryptography.fernet import Fernet
 if '--env' in sys.argv:
     env = sys.argv[sys.argv.index('--env') + 1]
     config = dotenv_values(env)
-elif len(dotenv_values()) > 0:
 # if a .env file exists
+elif len(dotenv_values()) > 0:
+    env = ".env"
     config = dotenv_values()
     encpwdbyt = bytes(config['encrypted_password'], 'utf-8')
     refKeybyt = bytes(config['key'], 'utf-8')
     keytouse = Fernet(refKeybyt)
-    config["sn_dev_password"] = (Fernet(refKeybyt).decrypt(encpwdbyt)).decode("utf-8") 
+    config["sn_dev_password"] = (
+        Fernet(refKeybyt).decrypt(encpwdbyt)).decode("utf-8")
 
 elif len(dotenv_values()) == 0:
     # if a .env file does not exist, create one
     env = ".env"
     # get username and password from user
-    sn_dev_username = input("Please Enter Username (SN Dev Portal Email):") 
-    sn_dev_password = getpass.getpass("Please Enter Password (SN Dev Portal Password):") 
+    sn_dev_username = input("Please Enter Username (SN Dev Portal Email):")
+    sn_dev_password = getpass.getpass(
+        "Please Enter Password (SN Dev Portal Password):")
 
     # generate key to encrypt passord
     key = Fernet.generate_key()
@@ -51,11 +56,12 @@ elif len(dotenv_values()) == 0:
     # convert into byte
     mypwdbyt = bytes(sn_dev_password, 'utf-8')
     encrypted_password = refKey.encrypt(mypwdbyt)
- 
+
     # save into new .env file
     with open(env, "w") as dot_env_file:
         dot_env_file.write("sn_dev_username=" + sn_dev_username + "\n")
-        dot_env_file.write("encrypted_password=" + encrypted_password.decode("utf-8")  + "\n")
+        dot_env_file.write("encrypted_password=" +
+                           encrypted_password.decode("utf-8") + "\n")
         dot_env_file.write("key=" + key.decode("utf-8") + "\n")
         dot_env_file.close()
 
@@ -65,7 +71,8 @@ elif len(dotenv_values()) == 0:
 silent = '--silent' in sys.argv
 
 if not silent:
-    print(datetime.today().strftime("%Y-%m-%d"), datetime.now().strftime("%H:%M:%S"), "SN Dev username:", config["sn_dev_username"])
+    print(datetime.today().strftime("%Y-%m-%d"), datetime.now().strftime("%H:%M:%S"),
+          "SN Dev username :", config["sn_dev_username"])
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -73,25 +80,27 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # display.start()
 
 # Signin to instance via headless chromium to wake up.
+
+
 def wake(config):
     sn_dev_username = config["sn_dev_username"]
     sn_dev_password = config["sn_dev_password"]
-    if "instance_name" in config:
-        instance_name = config["instance_name"]
-    else:
-        instance_name = ''
-    
+    instance_name = config["instance_name"] if "instance_name" in config else ""
+    instance_release = config["instance_release"] if "instance_release" in config else ""
+
     chrome_options = Options()
     if '--not-headless' not in sys.argv:
         chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-extensions")                                                                                                                                  
-    chrome_options.add_argument("--start-maximized")                                                                                                                                  
-    chrome_options.add_argument("--window-size=1400,800")                                                                                                                                  
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--window-size=1400,800")
 
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option(
+        "excludeSwitches", ["enable-logging"])
+    chrome_options.add_experimental_option(
+        "excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option("useAutomationExtension", False)
 
     os.environ["WDM_SSL_VERIFY"] = "0"
@@ -99,8 +108,9 @@ def wake(config):
 
     dev_portal_url = "https://developer.servicenow.com/dev.do#!/home"
     signon_url = "https://signon.service-now.com/ssologin.do?RelayState=%252Fapp%252Fservicenow_ud%252Fexks6phcbx6R8qjln0x7%252Fsso%252Fsaml%253FRelayState%253Dhttps%25253A%25252F%25252Fdeveloper.servicenow.com%25252Fnavpage.do&redirectUri=&email="
-    
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+    driver = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()), options=chrome_options)
     driver.get(signon_url)
     # Sign In
     # username input box
@@ -120,7 +130,7 @@ def wake(config):
     WebDriverWait(driver, 10).until(
         EC.url_to_be(dev_portal_url)
     )
-    # wait for header avatar to be available 
+    # wait for header avatar to be available
     avatar_query = "return document.querySelector('dps-app').shadowRoot.querySelector('div').querySelector('header').querySelector('dps-navigation-header').shadowRoot.querySelector('header>div>div>ul>li>dps-login')"
     WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable(driver.execute_script(avatar_query))
@@ -132,11 +142,13 @@ def wake(config):
     instance_status = ''
     while instance_status != 'Online':
         status_query = "return document.querySelector('dps-app').shadowRoot.querySelector('div').querySelector('header').querySelector('dps-navigation-header').shadowRoot.querySelector('header').querySelector('dps-navigation-header-dropdown').querySelector('dps-navigation-login-management').shadowRoot.querySelector('dps-navigation-header-dropdown-content').querySelector('dps-navigation-section').querySelector('dps-navigation-instance-management').shadowRoot.querySelector('dps-content-stack')"
-        instance_status = driver.execute_script(status_query).text.split('\n')[1]
+        instance_status = driver.execute_script(
+            status_query).text.split('\n')[1]
         if instance_status != 'Online':
             time.sleep(100)
-    if not silent:
-        print(datetime.today().strftime('%Y-%m-%d'), datetime.now().strftime("%H:%M:%S"), "Instance status:", instance_status)
+    release_query = "return document.querySelector('dps-app').shadowRoot.querySelector('div').querySelector('header').querySelector('dps-navigation-header').shadowRoot.querySelector('header').querySelector('dps-navigation-header-dropdown').querySelector('dps-navigation-login-management').shadowRoot.querySelector('dps-navigation-header-dropdown-content').querySelectorAll('dps-navigation-section')[0]"
+    instance_release = driver.execute_script(release_query).text.split(
+        "\n")[driver.execute_script(release_query).text.split("\n").index("RELEASE") + 1]
 
     # get instance name (only available when instance is online)
     if instance_name == '':
@@ -144,24 +156,37 @@ def wake(config):
         WebDriverWait(driver, 10).until(
             # we find the instance name by clicking on Manage Password item in the Dev portal header menu
             # which is the 6th element in that list
-            EC.element_to_be_clickable(driver.execute_script(instance_name_query).find_element(By.XPATH, "li[6]"))
+            EC.element_to_be_clickable(driver.execute_script(
+                instance_name_query).find_element(By.XPATH, "li[6]"))
         )
-        driver.execute_script(instance_name_query).find_element(By.XPATH, "li[6]").click()
+        driver.execute_script(instance_name_query).find_element(
+            By.XPATH, "li[6]").click()
         time.sleep(2)
-        instance_name = driver.execute_script("return document.querySelector('dps-app').shadowRoot.querySelector('div').querySelector('header').querySelector('dps-instance-modal').shadowRoot.querySelector('dps-modal')").text.split('\n')[20]
+        instance_name_result = driver.execute_script(
+            "return document.querySelector('dps-app').shadowRoot.querySelector('div').querySelector('header').querySelector('dps-instance-modal').shadowRoot.querySelector('dps-modal')").text.split('\n')[20]
+        instance_name = instance_name_result.split(': ')[1]
         # save into current .env file
         with open(env, "a") as dot_env_file:
-            dot_env_file.write("instance_name=" + instance_name.split(': ')[1] + "\n")
+            dot_env_file.write("instance_name=" +
+                               instance_name + "\n")
+            dot_env_file.write("instance_release=" +
+                               instance_release + "\n")
             dot_env_file.close()
     if not silent:
-        print(datetime.today().strftime('%Y-%m-%d'), datetime.now().strftime("%H:%M:%S"), "Instance name  :", instance_name)
+        print(datetime.today().strftime('%Y-%m-%d'),
+              datetime.now().strftime("%H:%M:%S"), "Instance name   :", instance_name)
+        print(datetime.today().strftime('%Y-%m-%d'),
+              datetime.now().strftime("%H:%M:%S"), "Instance release:", instance_release)
+        print(datetime.today().strftime('%Y-%m-%d'),
+              datetime.now().strftime("%H:%M:%S"), "Instance status :", instance_status)
 
     # Save Img of signin proof.
     # driver.get_screenshot_as_file("capture.png")
     # print(datetime.today().strftime('%Y-%m-%d'), datetime.now().strftime("%H:%M:%S"), "Done, cleaning up.")
-    
+
     # Cleanup active browser.
     driver.quit()
+
 
 def sunsup():
     wake(config)
@@ -172,5 +197,6 @@ def sunsup():
 # while True:
 #     schedule.run_pending()
 #     time.sleep(1)
+
 
 sunsup()
